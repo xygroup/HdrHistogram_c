22fa0d83426b7fb52da9729a51d67cc7b029349e
Erik van der Tier <mail@erikvandertier.com>
2016-09-02 21:49:22 +0200

    Added package.cmake

---
+++ PACKAGE.cmake
@@ -0,0 +1,5 @@
+set (HDR_HISTOGRAM_INC ${INC_ROOT}/HdrHistogram_c/_/hdr_histogram)
+set (HDR_HISTOGRAM_LIB ${BAZEL_BIN}/HdrHistogram_c/libhdr_histogram_impl.a)
+
+include_directories(${HDR_HISTOGRAM_INC})
+
