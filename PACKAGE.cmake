e6e3ae4d4c60ca0231e257dd8b04bc8705779abe
Erich Ocean <erich.ocean@me.com>
2019-01-01 13:18:48 -0700

    removed CMake stuff

--- PACKAGE.cmake
+++
@@ -1,5 +0,0 @@
-set (HDR_HISTOGRAM_INC ${INC_ROOT}/hdr_histogram/_/hdr_histogram)
-set (HDR_HISTOGRAM_LIB ${BAZEL_BIN}/hdr_histogram/libhdr_histogram_impl.a)
-
-include_directories(${HDR_HISTOGRAM_INC})
-
